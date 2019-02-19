cf6f8aed3a6caa012651441836f5276926ccf16c
jiage <739164391@qq.com>
2019-02-19 17:28:37 +0800

    新建的User.java和前端页面

---
+++ day93-git_project/src/main/java/com/git/domain/User.java
@@ -0,0 +1,13 @@
+package com.git.domain;
+
+/**
+ * @Package: com.git.domain
+ * @author: JiaGe
+ * @date: 2019/2/19  17:19
+ */
+
+public class User {
+
+    private Integer id;
+    private String name;
+}
